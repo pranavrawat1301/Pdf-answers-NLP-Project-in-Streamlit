90d92a4ab840354cf928386cc9bb7d873dbc33d1
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2024-03-20 11:40:27 +0530

    Update NLP_streamlit.py

--- NLP_streamlit.py
+++ NLP_streamlit.py
@@ -83,7 +83,7 @@ def get_conversational_chain():
 
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001", google_api_key=GOOGLE_API_KEY)
     
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
