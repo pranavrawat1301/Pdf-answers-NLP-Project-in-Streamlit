9b070012321dda1ef4a786d8ff48f2fa571dc6f0
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2024-04-23 03:10:38 +0530

    Update NLP_streamlit.py

--- NLP_streamlit.py
+++ NLP_streamlit.py
@@ -22,7 +22,7 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 
-GOOGLE_API_KEY = "AIzaSyCRFEpeNoSTCA7IUqDvoP5oE245y_j9_xE"
+GOOGLE_API_KEY = "AIzaSyCyHF1McFZZkY-uWG6xsTwlcOOqzIR6uK8"
 
 # Set Google API key directly
 #os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
