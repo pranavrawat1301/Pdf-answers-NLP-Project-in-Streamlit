733387638621bd17120f318a61ceadecf832be81
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2024-08-08 01:28:43 +0530

    Update NLP_streamlit.py

--- NLP_streamlit.py
+++ NLP_streamlit.py
@@ -15,12 +15,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-#from langchain.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from langchain_community.vectorstores import FAISS
+#from langchain_community.vectorstores import FAISS
 
 GOOGLE_API_KEY = "AIzaSyCRFEpeNoSTCA7IUqDvoP5oE245y_j9_xE"
 
